9da6acee6b29a5dab257c286553125e7cb802514
Paul <wip321@yahoo.com>
2016-03-05 13:14:46 -0600

    add music form appears and fucking dissapears like magic

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementById('addMusic').addEventListener("click", function() {
+  document.getElementById('options').style.visibility = "hidden"
+  document.getElementById('songlist').style.visibility = "hidden"
+  document.getElementById('musicForm').style.visibility = "visible"
+})
+document.getElementById('viewMusic').addEventListener("click", viewMusic {
+  document.getElementById('options').style.visibility = "visible"
+  document.getElementById('songlist').style.visibility = "visible"
+  document.getElementById('musicForm').style.visibility = "hidden"
+})
